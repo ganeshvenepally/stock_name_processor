8bea8bf729519763d1d9d712b2e14763271af483
Ganesh Venepally <ganesh.venepally@gmail.com>
2023-10-08 09:27:28 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+def process_stock_names(data):
+    lines = data.split("\n")
+    stock_names = [line.replace("BSE:", "") + ".BO" for line in lines if "BSE:" in line]
+    return ", ".join(stock_names)
+
+st.title('Stock Name Processor')
+st.write('Input stock data in the provided format and get processed stock names.')
+
+# User input
+data = st.text_area("Enter stock data:")
+
+if st.button('Process Stock Names'):
+    processed_names = process_stock_names(data)
+    st.write(processed_names)
